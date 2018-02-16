e4444693e2fe14baff83443cfd5e19d68f66e364
xm39 <tony.jones@tesco.com>
2018-02-16 00:09:29 +0000

    Introduce separatorsToSystem method.

--- src/main/java/app/PhotoRenameApp.java
+++ src/main/java/app/PhotoRenameApp.java
@@ -7,13 +7,13 @@ import java.io.File;
 public class PhotoRenameApp {
     public static void main(String[] args) {
         if (args.length == 0) {
-            run(new PhotoFolder(new File("/Users/tonyjones/Family Video Library"),
-                            "/Users/tonyjones",
+            run(new PhotoFolder(new File(separatorsToSystem("/Users/tonyjones/Family Video Library")),
+                            separatorsToSystem("/Users/tonyjones"),
                             false),
                     true);
         } else {
             run(new PhotoFolder(new File(args[0]),
-                            "/Users/tonyjones",
+                            separatorsToSystem("/Users/tonyjones"),
                             translateUpdateParameter(args[2])),
                     translateUpdateParameter(args[1]));
         }
@@ -27,5 +27,16 @@ public class PhotoRenameApp {
 
     private static boolean translateUpdateParameter(String arg) {
         return arg.toUpperCase().equals("Y");
+    }
+
+    private static String separatorsToSystem(String path) {
+        if (path==null) return null;
+        if (File.separatorChar=='\\') {
+            // From Windows to Linux/Mac
+            return path.replace('/', File.separatorChar);
+        } else {
+            // From Linux/Mac to Windows
+            return path.replace('\\', File.separatorChar);
+        }
     }
 }
